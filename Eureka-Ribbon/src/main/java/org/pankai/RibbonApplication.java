cd924b8017b108247d011c161347a2cdb4232bf6
Kai Pan <Kai Pan>
2016-10-21 10:36:32 +0800

    add Hystrix Dashboard

--- Eureka-Ribbon/src/main/java/org/pankai/RibbonApplication.java
+++ Eureka-Ribbon/src/main/java/org/pankai/RibbonApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class RibbonApplication {
 
     public static void main(String[] args) {
