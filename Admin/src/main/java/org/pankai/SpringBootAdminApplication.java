fc75de194c651fafd6494777ae5e49a191a3bced
Kai Pan <Kai Pan>
2016-11-03 11:26:30 +0800

    springboot-admin-module.

---
+++ Admin/src/main/java/org/pankai/SpringBootAdminApplication.java
@@ -0,0 +1,19 @@
+package org.pankai;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@EnableDiscoveryClient
+public class SpringBootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+
+}
